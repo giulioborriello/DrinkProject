fcb6508520a0c2c3d3a9de32d70fabe9b0a34a46
Cavaliere <s.lorenzo@studenti.unina.it>
2023-04-06 18:57:28 +0200

    Costruzione librerie per Server non monolitico

--- ServerC/DaoPostgres.c
+++ ServerC/DaoPostgres.c
@@ -2,9 +2,54 @@
 #include <stdio.h>  
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SOCKETERROR (-1)
 
 void* handle_connection(void* client_socket){
+    int client_socket = *((int *)p_client_socket);
+    free(p_client_socket);
+    char buffer[BUFSIZ];
+    size_t bytes_read;
+    int msgsize = 0;
 
+    //read client message -- the name of the file to read
+    while ((bytes_read= read(client_socket, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0){
+        msgsize += bytes_read;
+        if (msgsize > BUFSIZE-1 || buffer[msgsize-1] == '\n') break;
+    }
+    check(bytes_read, "recv error");
+    buffer[msgsize-1] = 0; //null termina il messaggio e rimuove \name
+
+    printf("REQUEST: %s\n",buffer);
+    fflush(stdout);
+
+    //validity check
+    if (realpath(buffer, actualpath) == NULL)
+    {
+        printf("ERROR(Bad path): %s\n", buffer);
+        close(client_socket);
+        return NULL;
+    }
+    
+    //read file and send its contents to client 
+    FILE *fp = fopen(actualpath, "r");
+    if (fp == NULL)
+    {
+        printf("ERROR(open): %s\n",buffer);
+        close(client_socket);
+        return NULL;
+    }
+
+    while ((bytes_read = fread(buffer, 1, BUFSIZE, fp)) > 0)
+    {
+        printf("Sending %zu bytes\n",bytes_read);
+        write(client_socket, buffer, bytes_read);
+    }
+    close(client_socket);
+    fclose(fp);
+    printf("Closing Connection.\n");
 }
 
 /// @brief controlla se l'operazione causa errori
@@ -117,7 +162,29 @@ void querySQL (char *query){
       }
       printf("\n");
    }
-   ss
+   
    // Libera la memoria
    PQclear(res);
+}
+
+//* funzione void che esegue una insert su postgres database
+void insertSQL(char *query){
+    res = PQexec(conn, query);
+   
+   // Controllo la query
+   if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+      printf("Errore nella query: %s", PQerrorMessage(conn));
+      PQclear(res);
+      PQfinish(conn);
+      exit(1);
+   }
+   printf("\nIstruzione di inserimento eseguita con successo.\n");
+   
+   // Libera la memoria
+   PQclear(res);
+}
+
+//*funzione void che chiude la connessione con il database
+void closeSQL(){
+    PQfinish(conn);
 }
