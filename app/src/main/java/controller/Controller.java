76d36dd79535e1f473b4e77ce780de28f891eced
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2023-05-07 16:15:25 +0200

    implementata dump dati, login e signIn, in attesa di test

--- app/src/main/java/controller/Controller.java
+++ app/src/main/java/controller/Controller.java
@@ -6,52 +6,53 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import  model.*;
+import Dao.Connessione;
+import model.*;
 
 @SuppressWarnings("unused")
 public class Controller {
     private static Controller controller;
-    private static Utente utente=null;
-    private static final ArrayList<Drink> listaDeiDrink= new ArrayList<>();
-    private static final ArrayList<String> categorie= new ArrayList<>();
+    private static Utente utente = null;
+    private static final ArrayList<Drink> listaDeiDrink = new ArrayList<>();
+    private static final ArrayList<String> categorie = new ArrayList<>();
 
-    private Controller (){
+    private Controller() {
         dump();
     }
 
-    public boolean ilCarrelloéVuoto(){
-        if(utente.getDrinkOrdineList().size() == 0)
+    public boolean ilCarrelloéVuoto() {
+        if (utente.getDrinkOrdineList().size() == 0)
             return true;
         return utente.getDrinkOrdineList().get(0).getDrink() == null;
     }
 
 
-    public static Controller getInstance(){
-        if(controller==null){
-            controller=new Controller();
+    public static Controller getInstance() {
+        if (controller == null) {
+            controller = new Controller();
         }
         return controller;
     }
 
 
-    private byte[] getByteArray(){
+    private byte[] getByteArray() {
         byte[] array;
-            try {
-                array = Files.readAllBytes(Paths.get("images.png"));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            array = Files.readAllBytes(Paths.get("images.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return array;
     }
 
 
-    private void fakeDump(){
-        login("GLR","pass");
+    private void fakeDump() {
+        login("GLR", "pass");
         listaDeiDrink.add(new Drink("1",
-                    "Mojito",
-                    "Alcolici",
-                    "\tUn cocktail a base di rum, lime, zucchero di canna, soda e foglie di menta",
-                    7.5));
+                "Mojito",
+                "Alcolici",
+                "\tUn cocktail a base di rum, lime, zucchero di canna, soda e foglie di menta",
+                7.5));
 
         listaDeiDrink.add(new Drink("2",
                 "Spritz",
@@ -64,7 +65,6 @@ public class Controller {
                 "Analcolici",
                 "Acqua naturale o frizzante",
                 1.5));
-
 
 
         listaDeiDrink.add(new Drink("4",
@@ -86,126 +86,190 @@ public class Controller {
     }
 
 
-    public void dump(){
+    public void dump() {
         //TODO fare il dump dei drink
+/*      Connessione con;
+        try {
+            con = Connessione.getInstance();
+            String querySelectForDrink = "SELECT * " +
+                    "FROM drink";
+            List<String> tabellaDrink = con.sendSelect(querySelectForDrink);
+            for (String eleDrink : tabellaDrink
+            ) {
+                System.out.println(eleDrink);
+            }
+
+            String querySelectForCategorie = "SELECT DISTINCT categoria" +
+                    "FROM drink";
+            List<String> listaCategorie = con.sendSelect(querySelectForCategorie);
+            categorie.add("all");
+            categorie.addAll(listaCategorie);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+         try {
+            con.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+*/
+
         fakeDump();
-        System.out.println("drink 1: "+getDrink("1").toString());
-        System.out.println("drink 2: "+getDrink("2"));
+        System.out.println("drink 1: " + getDrink("1").toString());
+        System.out.println("drink 2: " + getDrink("2"));
+
     }
 
 
     public boolean login(String username, String password) {
         //check if username and password are valid
         //TODO verificare login in c e ricevere i suoi dati
-        /* noinspection
-        Connessione conn = null;
+
+
+        Connessione conn;
         try {
             conn = Connessione.getInstance();
         } catch (IOException e) {
             System.err.println("Non sono riuscito a connettermi a Server");
-            throw new RuntimeException(e);
+            return false;
         }
-        conn.sendMessage("salutare server c");
-        */
-        utente = new Utente("0","GLR","LSO","GLR.unina.it","pass");
+        String querySelectUtente = "SELECT * " +
+                "FROM utente" +
+                "WHERE email=' " + username + "' AND" +
+                "password ='" + password + "'";
+        List<String> utenteRes;
+        try {
+            utenteRes = conn.sendSelect(querySelectUtente);
+        } catch (IOException e) {
+            System.err.println("Non sono riuscito a fare il login");
+            return false;
+        }
+        //TODO DATO LA LISTA PRENDERE I VALORI AL SUO INTERNO e creare oggetto utente
+
+        utente = new Utente("0", "GLR", "LSO", "GLR.unina.it", "pass");
 
         return true;
     }
 
 
-    public boolean signIn(String name, String surname, String username, String password){
+    public boolean signIn(String name, String surname, String username, String password) {
         //TODO fare il signin
+        /*
+        INSERT INTO public.utente(
+	        id, nome, cognome, email, password, "dati_Biometrici")
+	        VALUES (?, ?, ?, ?, ?, ?);
+         */
+        String queryInsertUtente = "INSERT INTO utente" +
+                "(nome, cognome email, password)" +
+                "VALUES (" +
+                "'" + name+ "'," + "'" + surname+ "'," + "'" + username+ "'," + "'" + password+"'" +
+                ");";
+/*
+        try {
+            if( (Connessione.getInstance().sendInsert(queryInsertUtente)) ){
+                return true;
+                utente = new Utente("0",name,surname,username,password);
+            }else{
+                return false;
+            }
+        } catch (IOException e) {
+            return  false;
+        }
+
+*/
         return true;
     }
 
 
-    public boolean logout(){
+    public boolean logout() {
         //TODO fare il logout
-        utente=null;
+        utente = null;
         return true;
     }
 
 
-    public ArrayList<Drink> getDrinks(){
+    public ArrayList<Drink> getDrinks() {
         return listaDeiDrink;
     }
 
 
-
-    public Drink getDrink(String idDrink){
-        for (Drink drink:listaDeiDrink
+    public Drink getDrink(String idDrink) {
+        for (Drink drink : listaDeiDrink
         ) {
-            if(drink.getId().equals(idDrink)){
+            if (drink.getId().equals(idDrink)) {
                 return drink;
             }
 
         }
         return null;
     }
-    public ArrayList<Drink> cercaDrink(String parolaCercata){
-        ArrayList<Drink> listaDeiDrinkCercati = new ArrayList<>();
-        parolaCercata=parolaCercata.toLowerCase();
 
-        for (Drink drink: listaDeiDrink
-             ) {
-                if ( drink.getNome().toLowerCase().contains(parolaCercata)|| drink.getCategoria().toLowerCase().contains(parolaCercata)) listaDeiDrinkCercati.add(drink);
+    public ArrayList<Drink> cercaDrink(String parolaCercata) {
+        ArrayList<Drink> listaDeiDrinkCercati = new ArrayList<>();
+        parolaCercata = parolaCercata.toLowerCase();
+
+        for (Drink drink : listaDeiDrink
+        ) {
+            if (drink.getNome().toLowerCase().contains(parolaCercata) || drink.getCategoria().toLowerCase().contains(parolaCercata))
+                listaDeiDrinkCercati.add(drink);
         }
 
-    return listaDeiDrinkCercati;
+        return listaDeiDrinkCercati;
     }
 
-    public ArrayList<Drink> FiltraDrinkPerCategoria(String categoriaSelezionata){
-            ArrayList<Drink> listaDeiDrinkCercati = new ArrayList<>();
-            categoriaSelezionata= categoriaSelezionata.toLowerCase();
-            for (Drink drink: listaDeiDrink
-            ) {
-                if ( drink.getCategoria().toLowerCase().matches(categoriaSelezionata)) listaDeiDrinkCercati.add(drink);
-            }
+    public ArrayList<Drink> FiltraDrinkPerCategoria(String categoriaSelezionata) {
+        ArrayList<Drink> listaDeiDrinkCercati = new ArrayList<>();
+        categoriaSelezionata = categoriaSelezionata.toLowerCase();
+        for (Drink drink : listaDeiDrink
+        ) {
+            if (drink.getCategoria().toLowerCase().matches(categoriaSelezionata))
+                listaDeiDrinkCercati.add(drink);
+        }
 
-            return listaDeiDrinkCercati;
+        return listaDeiDrinkCercati;
     }
 
 
-
-
-    public void addDrink(String idDrinkOrdinato, int quantita){
+    public void addDrink(String idDrinkOrdinato, int quantita) {
         //controllo quantita '
-        if(quantita<=0) return;
+        if (quantita <= 0) return;
 
         //recupera drink
-        Drink drinkDaAggiungere=getDrink(idDrinkOrdinato);
+        Drink drinkDaAggiungere = getDrink(idDrinkOrdinato);
 
-        if(drinkDaAggiungere==null) return;
+        if (drinkDaAggiungere == null) return;
 
-        utente.addDrink(drinkDaAggiungere,quantita);
+        utente.addDrink(drinkDaAggiungere, quantita);
     }
 
 
-    public boolean removeDrink(String idDrink){
+    public boolean removeDrink(String idDrink) {
         //recupera drink
-        Drink idDrinkDaRimuovere=getDrink(idDrink);
+        Drink idDrinkDaRimuovere = getDrink(idDrink);
 
-        if(idDrinkDaRimuovere==null) return false;
+        if (idDrinkDaRimuovere == null) return false;
 
         utente.removeDrink(idDrinkDaRimuovere);
         return true;
     }
 
 
-    public boolean removeDrink(String idDrink, int quantity){
+    public boolean removeDrink(String idDrink, int quantity) {
         //recupera drink
-        Drink idDrinkDaRimuovere=getDrink(idDrink);
+        Drink idDrinkDaRimuovere = getDrink(idDrink);
 
-        if(idDrinkDaRimuovere==null) return false;
+        if (idDrinkDaRimuovere == null) return false;
 
-        utente.removeDrink(idDrinkDaRimuovere,quantity);
+        utente.removeDrink(idDrinkDaRimuovere, quantity);
         return true;
     }
 
-    public String getIdDrinkByName(String name){
-        for (Drink drink:listaDeiDrink
+    public String getIdDrinkByName(String name) {
+        for (Drink drink : listaDeiDrink
         ) {
-            if(drink.getNome().equals(name)){
+            if (drink.getNome().equals(name)) {
                 return drink.getId();
             }
 
@@ -213,53 +277,55 @@ public class Controller {
         return null;
     }
 
-    public boolean updateDrink(String idDrink, int quantita){
+    public boolean updateDrink(String idDrink, int quantita) {
         Drink drinkDaAggiornare = getDrink(idDrink);
-        if(!esisteIlDrinkNelCarrello(idDrink))
-            addDrink(idDrink,quantita);
+        if (!esisteIlDrinkNelCarrello(idDrink))
+            addDrink(idDrink, quantita);
         else
-            utente.updateQuantita(drinkDaAggiornare,quantita);
+            utente.updateQuantita(drinkDaAggiornare, quantita);
         return true;
     }
 
     private boolean esisteIlDrinkNelCarrello(String idDrink) {
-        for (DrinkOrdine drinkOrdine: utente.getDrinkOrdineList()) {
-            if(drinkOrdine.getDrink() == null) break;
-            if(drinkOrdine.getDrink().getId().equals(idDrink)) return true;
+        for (DrinkOrdine drinkOrdine : utente.getDrinkOrdineList()) {
+            if (drinkOrdine.getDrink() == null) break;
+            if (drinkOrdine.getDrink().getId().equals(idDrink)) return true;
         }
         return false;
     }
 
-    public List<DrinkOrdine> getSummary(){
+    public List<DrinkOrdine> getSummary() {
         return utente.getDrinkOrdineList();
     }
 
-    public String getPrezzoTotale(){
+    public String getPrezzoTotale() {
         return String.valueOf(utente.getPrezzoTotale());
     }
-    boolean EffettuaPagamento(){
-        String queryClone="INSERT INTO public.drink_ordine(\n" +
-                "\t drink_id, ordine_id, quantita, prezzo)\n"+
+
+    boolean EffettuaPagamento() {
+        String queryClone = "INSERT INTO public.drink_ordine(\n" +
+                "\t drink_id, ordine_id, quantita, prezzo)\n" +
                 "\tVALUES\n";
-                //"\tVALUES ( ?, ?, ?, ?) "
-        StringBuilder query= new StringBuilder();
+        //"\tVALUES ( ?, ?, ?, ?) "
+        StringBuilder query = new StringBuilder();
         ArrayList<DrinkOrdine> carello = (ArrayList<DrinkOrdine>) utente.getDrinkOrdineList();
-        for (DrinkOrdine drinkOrdinato: carello) {
-            String drink_id=drinkOrdinato.getDrink().getId();
-            String drink_ordine="0";//TODO crea la query per creare ordine cosi da avere id del ordine
-            int quantita= drinkOrdinato.getQuantita();
-            double prezzo= drinkOrdinato.getPrezzo();
-            String value= "\t(" + drink_id + ","+drink_ordine+"," + quantita + "," + prezzo + ");\n";
+        for (DrinkOrdine drinkOrdinato : carello) {
+            String drink_id = drinkOrdinato.getDrink().getId();
+            String drink_ordine = "0";//TODO crea la query per creare ordine cosi da avere id del ordine
+            int quantita = drinkOrdinato.getQuantita();
+            double prezzo = drinkOrdinato.getPrezzo();
+            String value = "\t(" + drink_id + "," + drink_ordine + "," + quantita + "," + prezzo + ");\n";
             query.append(queryClone).append(value);
         }
-        System.out.println("sono nel metodo Controller.EffettuaPagamento()\n la query creata è:\n"+query);
+        System.out.println("sono nel metodo Controller.EffettuaPagamento()\n la query creata è:\n" + query);
         //TODO inserire logica comunicazione server c ;
+        boolean pagamentoEffettuato = true;//da cambiare in base all'esito del server
 
-        return true;
+        return pagamentoEffettuato;
     }
 
 
-    public String[] getCategorie(){
+    public String[] getCategorie() {
         String[] categorieArray = new String[categorie.size()];
         for (String category : categorie) {
             categorieArray[categorie.indexOf(category)] = category;
